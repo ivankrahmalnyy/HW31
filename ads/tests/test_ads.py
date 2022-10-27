d6cd3c166c60056fd7d63eccedd7c95f2ce4c4f1
Ivan <ifucin@gmail.com>
2022-10-27 19:16:14 +0300

    Commit_31

--- ads/tests/test_ads.py
+++ ads/tests/test_ads.py
@@ -13,7 +13,7 @@ def test_create_ad(api_client, user):
         "author": user.id,
         "price": 10
     }
-    url = reverse("ad_create")
+    url = reverse('ad_create')
     res = api_client.post(
         url,
         data=json.dumps(data),
@@ -27,7 +27,7 @@ def test_create_ad(api_client, user):
 
 
 @pytest.mark.django_db
-def test_list_ads(api_client, ad):
+def test_list_ads(api_client):
     url = reverse("ad_list")
     res = api_client.get(url)
     assert res.status_code == status.HTTP_200_OK
